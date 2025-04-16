77a934dad07c5f2ae51398dfd6b38939df3cc0ed
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-04-16 07:39:02 -0400

    added address model

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    street_number = models.CharField(max_length=50)
+    city_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return f"{self.street} {self.street_number}, {self.city}"
+
