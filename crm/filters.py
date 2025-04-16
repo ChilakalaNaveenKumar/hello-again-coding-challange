b9820512f15770705dd7210b537b36ef2877153f
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-04-16 17:07:13 -0400

    optimized the filters by modifying filter types and adding lookuo_exp

--- crm/filters.py
+++ crm/filters.py
@@ -10,8 +10,8 @@ class AppUserFilter(rest_framework.FilterSet):
     phone_number = rest_framework.CharFilter()
     customer_id = rest_framework.CharFilter()
     birthday = rest_framework.DateFilter()
-    created = rest_framework.DateTimeFilter()
-    last_updated = rest_framework.DateTimeFilter()
+    created = rest_framework.DateFilter()
+    last_updated = rest_framework.DateFilter()
 
     # Address fields
     address__street = rest_framework.CharFilter()
@@ -20,9 +20,30 @@ class AppUserFilter(rest_framework.FilterSet):
 
     # CustomerRelationship fields
     customerrelationship__points = rest_framework.NumberFilter()
-    customerrelationship__created = rest_framework.DateTimeFilter()
-    customerrelationship__last_activity = rest_framework.DateTimeFilter()
+    customerrelationship__created = rest_framework.DateFilter()
+    customerrelationship__last_activity = rest_framework.DateFilter()
 
     class Meta:
         model = AppUser
         fields = []
+        
+class OptimizedAppUserFilter(rest_framework.FilterSet):
+    # AppUser fields
+    first_name = rest_framework.CharFilter(lookup_expr='icontains')
+    last_name = rest_framework.CharFilter(lookup_expr='icontains')
+    gender = rest_framework.CharFilter()
+    phone_number = rest_framework.CharFilter(lookup_expr='icontains')
+    customer_id = rest_framework.CharFilter(lookup_expr='icontains')
+    birthday = rest_framework.DateFromToRangeFilter()
+    created = rest_framework.DateFromToRangeFilter()
+    last_updated = rest_framework.DateFromToRangeFilter()
+
+    # Address fields
+    address__street = rest_framework.CharFilter()
+    address__city = rest_framework.CharFilter()
+    address__country = rest_framework.CharFilter()
+
+    # CustomerRelationship fields
+    customerrelationship__points = rest_framework.RangeFilter()
+    customerrelationship__created = rest_framework.DateFromToRangeFilter()
+    customerrelationship__last_activity = rest_framework.DateFromToRangeFilter()
