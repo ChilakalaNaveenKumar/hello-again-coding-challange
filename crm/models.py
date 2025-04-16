0216cdd1c20a78213c459f18f51b859bc0f9c8c3
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-04-16 07:50:58 -0400

    added AppUser model

--- crm/models.py
+++ crm/models.py
@@ -10,3 +10,22 @@ class Address(models.Model):
     def __str__(self) -> str:
         return f"{self.street} {self.street_number}, {self.city}"
 
+class AppUser(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Others'),
+    )
+    
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    customer_id = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    birthday = models.DateField()
+    last_updated = models.DateTimeField(auto_now=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
