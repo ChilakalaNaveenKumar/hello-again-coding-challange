a9dc3e96b4fd8bd2cdcaefde054032cc0bc09970
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-04-18 06:38:39 -0400

    allowd host didnt work

--- django_performance_dashboard/vite.config.js
+++ django_performance_dashboard/vite.config.js
@@ -16,6 +16,9 @@ export default defineConfig({
         secure: false,
       }
     },
-    allowedHosts: 'all',
+    allowedHosts: ['.ngrok-free.app'], // ✅ allows all hosts including ngrok
+    host: '0.0.0.0',     // ✅ required for access outside the container
+    port: 5173,
+    strictPort: true     // ✅ ensures ngrok port stays fixed
   }
 })
