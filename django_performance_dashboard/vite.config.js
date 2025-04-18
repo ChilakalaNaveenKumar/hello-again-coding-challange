94aa52f9ce7b54cf483222ab6618f09207df6179
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-04-18 06:34:46 -0400

    allow all hosts

--- django_performance_dashboard/vite.config.js
+++ django_performance_dashboard/vite.config.js
@@ -13,8 +13,9 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
-    }
+    },
+    allowedHosts: 'all',
   }
 })
